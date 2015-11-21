7cbbf402f37a265470c543a6c130a7b11e7794ba
unknown <U6029671@UC182069-TPL-B.ten.thomsonreuters.com>
2015-11-22 05:10:02 +0530

    fixed details tab

--- scripts/mainController.js
+++ scripts/mainController.js
@@ -3,6 +3,9 @@ app.controller("mainController", function($scope,$http,$filter){
 	var api_hit_url = "https://zoomcar-ui.0x10.info/api/courier?type=json&query=api_hits";
 	var local_api_hit = "data/api_hits.json";
 	var local_parcel = "data/parcels.json";
+	// $scope.load_first = function(){
+	// 	document.getElementById("parcels_list").getElementsByTagName('a')[0].click();
+	// }
 // API Hits
 	$http.get(api_hit_url).success(function(response){
 		$scope.api_hits = response;
@@ -12,6 +15,9 @@ app.controller("mainController", function($scope,$http,$filter){
 	$http.get(parcel_url).success(function(response){
 		var parcelsData = response;
 		$scope.parcels_data = parcelsData.parcels;
+	})
+	.finally(function() {
+		document.getElementById("parcels_list").getElementsByTagName('a')[0].click();
 	});
 // Order
 	var orderBy = $filter('orderBy');
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,52 @@
-function update_details(e){
-	var data = e.getAttribute("data-name");
-	console.log(data);
+// initialise google maps
+function initMap(lat, lng) {
+  lat = typeof lat !== 'undefined' ? lat : "-25.363";
+  lng = typeof lng !== 'undefined' ? lng : "131.044";
+  var myLatLng = {lat: parseFloat(lat), lng: parseFloat(lng)};
+  var mapOptions = {
+    zoom: 16,
+    center: myLatLng,
+    disableDefaultUI: true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Current location'
+  });
 }
+
+function update_details(e){
+	var data_name = e.getAttribute("data-name");
+	var data_image = e.getAttribute("data-image");
+	var data_date = e.getAttribute("data-date");
+	var data_type = e.getAttribute("data-type");
+	var data_weight = e.getAttribute("data-weight");
+	var data_phone = e.getAttribute("data-phone");
+	var data_price = e.getAttribute("data-price");
+	var data_quantity = e.getAttribute("data-quantity");
+	var data_color = e.getAttribute("data-color");
+	var data_link = e.getAttribute("data-link");
+	var data_latitude = e.getAttribute("data-latitude");
+	var data_longitude = e.getAttribute("data-longitude");
+	document.getElementById('parcel_name').textContent= data_name;
+	document.getElementById('parcel_image').src = data_image;
+	document.getElementById('parcel_date').textContent = data_date;
+	document.getElementById('parcel_type').textContent = data_type;
+	document.getElementById('parcel_weight').textContent = data_weight;
+	document.getElementById('parcel_phone').textContent = data_phone;
+	document.getElementById('parcel_price').textContent = data_price;
+	document.getElementById('parcel_quantity').textContent = data_quantity;
+	document.getElementById('parcel_color').style.background = data_color;
+	initMap(data_latitude, data_longitude);
+
+	var d = new Date(parseInt(data_date));
+	console.log(d);
+	document.getElementById('parcel_date').textContent = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear();
+}
+window.onload = function() {
+	// setTimeout(function(){ document.getElementById("parcels_list").getElementsByTagName('a')[0].click(); }, 3000);
+};
